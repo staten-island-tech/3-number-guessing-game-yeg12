c8c5d7129e40b502852e06652f24accf13ebdaec
yeg12 <yeg@nycstudents.net>
2026-03-02 09:55:06 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
-def occupied (n,y,t ):
-    y[10]
-    t[5]
-    result=[y-t]
-    print(result)
+# def occupied (n,y,t ):
+#     found = 0
+#     for i in range(n):
+def lang (sent):
+    t = 0
+    s = 0
+    for i in sent:
+            if i == "s" or i  == "S":
+                s += 1
+            elif i == "t" or i =="T":
+                t +=1
+if "s">="t":
+     print ("french")
+else:
+     print ("English")
+
+lang("ttttttttttttttttttttttttttt")
