5396392c1a3f93b7ee5bfe0f2bf5504029de8f68
yeg12 <yeg@nycstudents.net>
2026-03-27 09:46:09 -0400

    xzc

--- test.py
+++ test.py
@@ -1,17 +1,57 @@
 # def occupied (n,y,t ):
 #     found = 0
 #     for i in range(n):
-def lang (sent):
-    t = 0
-    s = 0
-    for i in sent:
-            if i == "s" or i  == "S":
-                s += 1
-            elif i == "t" or i =="T":
-                t +=1
-if "s">="t":
-     print ("french")
-else:
-     print ("English")
+# def lang (sent):
+#     t = 0
+#     s = 0
+#     for i in sent:
+#             if i == "s" or i  == "S":
+#                 s += 1
+#             elif i == "t" or i =="T":
+#                 t +=1
+# if "s">="t":
+#      print ("french")
+# else:
+#      print ("English")
 
-lang("sssssssssssssssssssssssss")
+# lang("sssssssssssssssssssssssss")
+
+def word (RandomSTUFF):
+    H = 0
+    O = 0
+    N = 0
+    I = 0
+    for i in RandomSTUFF:
+        if i == "H":
+            H += 1
+        if H >= H:
+            if i == "O":
+                O += 1
+                
+        
+            
+
+         
+
+
+
+
+
+
+RandomSTUFF="hoashjyuadvawnihajddsgfvwagni"
+
+
+
+#         if i == "H":
+#             h += 1
+#         elif i == "O":
+#             o += 1
+#         elif i == "N":
+#             n += 1
+#         elif i == "I":
+#             i += 0
+
+
+
+
+
